d138d4a250406abac0b0faa66cfad2abc8dafe3f
Afiq Ilyasa Akmal <afiqilyasaakmal01@gmail.com>
2022-09-22 04:36:49 +0700

    menambahkan pesan kalo nontonnya dah banyak

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -7,9 +7,22 @@ from django.core import serializers
 # Create your views here.
 def show_mywatchlist(request):
     data_barang_mywatchlist = WishlistFilmKu.objects.all()
+    sudahDitonton = 0
+    belumDitonton = 0
+    pesan = ""
+    for i in data_barang_mywatchlist:
+        if i.film_watched == True:
+            sudahDitonton += 1
+        else:
+            belumDitonton += 1
+    if sudahDitonton >= belumDitonton:
+        pesan = "Selamat, kamu sudah banyak menonton!"
+    else:
+        pesan = "Wah, kamu masih sedikit menonton!"
     context ={
         'list_film': data_barang_mywatchlist,
-        'nama': 'Afiq Ilyasa Akmal'
+        'nama': 'Afiq Ilyasa Akmal',
+        'pesan': pesan
     }
     return render(request, "mywatchlist.html", context)
 
